07b51ab18e8655aab905ab57757817735d509465
hediinn <hediinn@gmail.com>
2021-10-27 11:46:40 +0100

    made counter into a class

--- counter.py
+++ counter.py
@@ -2,15 +2,17 @@ import msvcrt,sys
 
 i =0
 
-while True:
+class counter:
+
+    while True:
     
-    if msvcrt.kbhit():
-        ch=ord(msvcrt.getch())
-        if i ==10:
-            i=0 
-        if ch == 113:
-            sys.exit()
-        if ch == 32:
-            i=i+1
-            print(i)
+        if msvcrt.kbhit():
+            ch=ord(msvcrt.getch())
+            if i ==10:
+                i=0 
+            if ch == 113:
+                sys.exit()
+            if ch == 32:
+                i=i+1
+                print(i)
     
